e362739232cb9695cf0615f835706fb7c313bb29
Andrew <andreasohaodha@proton.me>
2023-06-25 00:58:10 +0100

    Added confession of theft lol

--- stopwatch.sh
+++ stopwatch.sh
@@ -1,4 +1,6 @@
 #!/bin/sh 
+# Simple stopwatch script that simply counts the elapsed time in seconds. 
+# I believe that I stole this code from somewhere many years ago and adapted it to my liking, but I unfortunately do not recall from where. 
 
 start=$(date +%s)
 
