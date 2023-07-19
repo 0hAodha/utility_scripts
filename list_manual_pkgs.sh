af9e407a9046de140adf1f0c9c93f11d3a024da9
Andrew <andreasohaodha@proton.me>
2023-07-19 19:33:52 +0100

    remove leftover -F flag

--- list_manual_pkgs.sh
+++ list_manual_pkgs.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 # one-liner to list just the names of packages manually-installed with xbps-install, with no version numbers etc
-xbps-query --list-manual-pkgs | awk -F '{gsub(/-[0-9]+.*$/, ""); print $0}'
+xbps-query --list-manual-pkgs | awk '{gsub(/-[0-9]+.*$/, ""); print $0}'
+
+# alternative sed version:
+# xbps-query --list-manual-pkgs | awk -F '{gsub(/-[0-9]+.*$/, ""); print $0}'
