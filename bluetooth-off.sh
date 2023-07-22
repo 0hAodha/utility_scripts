d453a818d1aaf574df9bb16e8b54af28d1a9407a
Andrew <andreasohaodha@proton.me>
2023-07-22 19:13:22 +0100

    replace sudo with doas in bluetooth scripts

--- bluetooth-off.sh
+++ bluetooth-off.sh
@@ -5,4 +5,5 @@ echo "Attempting to turn off bluetooth via bluetoothctl"
 bluetoothctl power off
 
 echo "Attempting to remove the symlink to the bluetoothd service in the /var/service/ directory"
-sudo rm /var/service/bluetoothd
+# sudo rm /var/service/bluetoothd
+doas rm /var/service/bluetoothd
--- bluetooth-on.sh
+++ bluetooth-on.sh
@@ -2,7 +2,8 @@
 # Simple script to enable bluetooth on my system
 
 echo "Attempting to create a symbolic link to the bluetoothd service in /var/service/"
-sudo ln -s /etc/sv/bluetoothd /var/service/ 
+# sudo ln -s /etc/sv/bluetoothd /var/service/ 
+doas ln -s /etc/sv/bluetoothd /var/service/ 
 
 echo "Attempting to turn on bluetooth via bluetoothctl"
 bluetoothctl power on
