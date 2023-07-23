ed2583927c306d785f16a245e14fbf94a0ee2819
Andrew <andreasohaodha@proton.me>
2023-07-23 14:01:24 +0100

    change formatting

--- screenshot.sh
+++ screenshot.sh
@@ -1,8 +1,6 @@
 #!/bin/sh 
 # Simple screenshot script with selection.
-
 filepath="/home/andrew/media/images/screenshots/$(date +%Y-%m-%d\ %H:%M:%S).png"
-maim --select --hidecursor                                  |                                              
-tee "$filepath" | xclip -selection clipboard -target image/png  
 
+maim --select --hidecursor | tee "$filepath" | xclip -selection clipboard -target image/png  
 notify-send "screenshot.sh" "Screenshot saved to: $filepath"
