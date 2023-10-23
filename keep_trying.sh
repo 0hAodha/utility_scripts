cfcb9dc30437fe959e52f5847bc12819935d2bf5
Andrew <andreasohaodha@proton.me>
2023-10-23 18:58:13 +0100

    make keep_trying.sh poll repeatedly to see if connection has gone down

--- keep_trying.sh
+++ keep_trying.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
-# one-lienr to attempt to connect to wifi network, and keep trying until it succeeds
-nmcli con up <insert_network_name_here> || while [ "$?" != "0" ]; do nmcli con up <insert_network_name_here>; done
+# simple script to fix my broken wifi 
+# it checks if wifi is connected every 5 seconds, and if not, repeatedly attempts to connect
+
+network="<network_name>"
+interface="<interface_name>"
+
+while [ true ]; do 
+    # if wifi is connected, do nothing for 5 seconds
+    if [ -n "$(nmcli connection show | grep '.*'"$network"'.*'"$interface"'.*')" ]; then
+        sleep 5
+    # else, wifi is not connected so attempt to connect to wifi network, and keep trying until it succeeds
+    else 
+        nmcli con up "$network" || while [ "$?" != "0" ]; do nmcli con up "$network"; done
+    fi
+done
