d45b8d746df3bb48b926c6d75ba15d54983d3ebb
Andrew <andreasohaodha@proton.me>
2023-11-01 11:29:43 +0000

    make autopape use an interval variable

--- autopape.sh
+++ autopape.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-# simple script to loop through each image in a directory and set said image as wallpaper for 1 minute
+# simple script to loop through each image in a directory and set said image as wallpaper for a specififed interval using feh
+
+interval="1m"
 
 while [ true ]
 do
     for img in *
     do
         feh --bg-fill "$img"
-        sleep 1m
+        sleep "$interval"
     done
 done
