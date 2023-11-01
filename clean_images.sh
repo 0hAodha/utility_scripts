b8d06ce140db404d0ee090f55ed3b606396ccb6f
Andrew <andreasohaodha@proton.me>
2023-11-01 11:35:37 +0000

    generalise clean_images.sh to deal with any file type

--- clean_images.sh
+++ clean_images.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# script for looping through each image in a directory, displaying it with pqiv, and upon the closing of the image viewer, asking the user whether or not to delete the image, and deleting if "y" received
+# script for looping through each file in a directory, opening it with mimeopen, and upon the closing of the file viewer, asking the user whether or not to delete the image, and deleting if "y" received
 
 for file in *
 do 
-    pqiv "$file" || rm "$file" && read -p "Delete file? [y/n]: " ans && [ "$ans" = "y" ] && rm "$file"
+    mimeopen "$file" && read -p "Delete file? [y/n]: " ans && [ "$ans" = "y" ] && rm "$file"
 done
