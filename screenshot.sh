50fb226ea5e4c045c98be126689b6025c4dc1fdb
Andrew <andreasohaodha@proton.me>
2023-11-01 11:37:47 +0000

    better comment in screenshot.sh

--- screenshot.sh
+++ screenshot.sh
@@ -1,5 +1,5 @@
 #!/bin/sh 
-# Simple screenshot script with selection.
+# Simple screenshot script with selection using maim.
 filepath="/home/andrew/media/images/screenshots/$(date +%Y-%m-%d\ %H:%M:%S).png"
 
 maim --select --hidecursor | tee "$filepath" | xclip -selection clipboard -target image/png  
