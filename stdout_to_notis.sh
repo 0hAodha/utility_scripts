283d58c5d250ba9bec0513132adb7092df06d706
Andrew <andreasohaodha@proton.me>
2023-11-21 10:32:49 +0000

    update to reflect renaming of stdout_to_notis.sh to stdin_to_notis.sh!

--- stdout_to_notis.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh 
-# small script to turn the stdout output of a program into notifications line-by-line
-# intended for use when a script is not being called from a terminal, but from a GUI application so that output can still be seen
-
-while read line
-do
-    notify-send "$line"
-done
