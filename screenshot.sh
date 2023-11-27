46ee39b61a0a3896cbda542b1c8852e8a4908ed0
Andrew <andreasohaodha@proton.me>
2023-11-27 23:13:04 +0000

    use $HOME variable instead of hardcoded path in screenshot.sh

--- screenshot.sh
+++ screenshot.sh
@@ -1,6 +1,6 @@
 #!/bin/sh 
 # Simple screenshot script with selection using maim.
-filepath="/home/andrew/media/images/screenshots/$(date +%Y-%m-%d\ %H:%M:%S).png"
+filepath="$HOME/media/images/screenshots/$(date +%Y-%m-%d\ %H:%M:%S).png"
 
 maim --select --hidecursor | tee "$filepath" | xclip -selection clipboard -target image/png  
 notify-send "$0" "Screenshot saved to: $filepath"
