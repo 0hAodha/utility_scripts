b8fbaabdf5e36d2b9807404255c5eccb28ba4206
Andrew <andreasohaodha@proton.me>
2023-12-28 01:13:26 +0000

    change 'ln -s' to 'ln --symbolic' in bluetooth-on.sh
    for readability and consistency purposes

--- bluetooth-on.sh
+++ bluetooth-on.sh
@@ -2,8 +2,8 @@
 # Simple script to enable bluetooth on Void Linux (using the runit init system)
 
 echo "Attempting to create a symbolic link to the bluetoothd service in /var/service/"
-# sudo ln -s /etc/sv/bluetoothd /var/service/ 
-doas ln -s /etc/sv/bluetoothd /var/service/ 
+# sudo ln --symbolic /etc/sv/bluetoothd /var/service/ 
+doas ln --symbolic /etc/sv/bluetoothd /var/service/ 
 
 echo "Attempting to turn on bluetooth via bluetoothctl"
 bluetoothctl power on
