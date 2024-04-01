c38eb9d6c38d032bfa529372306786b5ab507a79
Andrew <andreasohaodha@proton.me>
2024-04-01 01:44:51 +0100

    add interval flag to autopape.sh

--- autopape.sh
+++ autopape.sh
@@ -3,6 +3,18 @@
 
 interval="1m"
 
+
+while getopts 'i:' flag; do
+  case "${flag}" in
+        i) 
+            interval="${OPTARG}";;
+        *)
+            echo "Unrecognised option"
+            exit 1;;
+  esac
+done
+
+
 while [ true ]
 do
     for img in *
