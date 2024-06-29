010676ebec48c716bac0b18e5a860024f8964a94
Andrew <andreasohaodha@proton.me>
2024-06-29 20:05:27 +0100

    Add sync_music.sh

---
+++ sync_music.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Script to sync my Android phone's music library to the contents of my ~/media/music/ directory
+
+mountpoint="$HOME/mnt"
+source="$HOME/media/music"
+destination="$mountpoint/Internal shared storage/Music"
+
+mkdir "$mountpoint"
+aft-mtp-mount "$mountpoint"
+
+rsync --archive --delete "$source" "$destination"
+
+sync
+fusermount -u "$mountpoint"
