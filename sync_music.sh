394d59562b200084a95c49d26153d54523d9c75b
Andrew <andreasohaodha@proton.me>
2024-07-13 00:57:04 +0100

    Add trailing '/' to filepaths in sync_music.sh

--- sync_music.sh
+++ sync_music.sh
@@ -2,13 +2,13 @@
 # Script to sync my Android phone's music library to the contents of my ~/media/music/ directory
 
 mountpoint="$HOME/mnt"
-source="$HOME/media/music"
-destination="$mountpoint/Internal shared storage/Music"
+source="$HOME/media/music/"
+destination="$mountpoint/Internal shared storage/Music/"
 
 mkdir "$mountpoint"
 aft-mtp-mount "$mountpoint"
 
-rsync --archive --delete "$source" "$destination"
+rsync --archive --delete --info=progress2 "$source" "$destination"
 
 sync
 fusermount -u "$mountpoint"
