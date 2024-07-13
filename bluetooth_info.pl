9d27a1c0570a383e4b2c037d5120cab6eb0f1ca8
Andrew <andreasohaodha@proton.me>
2024-07-13 04:24:36 +0100

    Add bluetooth_info.pl

---
+++ bluetooth_info.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl -l
+
+use strict;
+use warnings;
+
+my $output = `bluetoothctl info` or die;
+
+my $mac_address = $1 if ($output =~ /^Device\s+(\S+)/) or die("N/A\n");
+my $device_name = $1 if ($output =~ /Name:\s+(.*)/);
+my $icon = $1 if ($output =~ /Icon:\s+(.*)/);
+my $battery_percentage = $1 if ($output =~ /Battery Percentage:\s+\S+\s\(([0-9]+)\)/);
+
+if ($icon eq "audio-headphones") {
+    printf("  ");
+}
+printf("$device_name $battery_percentage%%");
