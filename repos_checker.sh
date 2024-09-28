ea4a806aeac74298a5ed169e687033f16448e540
Andrew <andreasohaodha@proton.me>
2024-09-28 02:45:19 +0100

    [repos_checker.sh]: Make repository filepaths more readable
    Make 'pwd' output bold and prepend a newline

--- repos_checker.sh
+++ repos_checker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # script to find all the git repositories in the current directory (& sub-directories) and display their git statuses
 
-find . -name ".git" -type d -not -path "./.cache/*" -not -path "./.local/*" -exec sh -c '(cd {}/../; pwd; git status)' \;
+find . -name ".git" -type d -not -path "./.cache/*" -not -path "./.local/*" -exec sh -c '(cd {}/../; echo -e "\n\033[1m$(pwd)\033[0m"; git status)' \;
