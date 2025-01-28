3e25e0fd8d376a782d07f55921f3b3cc81500d95
Andrew <andreasohaodha@proton.me>
2025-01-28 04:30:13 +0000

    [vimshot.sh]: Merge x11_vimshot.sh and wl_vimshot.sh

--- src/vimshot.sh
+++ src/vimshot.sh
@@ -3,7 +3,12 @@
 # For use as part of my LaTeX note-taking workflow in Vim: create the figure environment, copy the file name, invoke this script, and select the region in the source material to be screenshotted.
 
 basename=$(basename "$0")
-filepath=$(wl-paste)
+
+if [ $(pgrep -x "Xorg") ]; then 
+    filepath=$(xclip -selection clipboard -out)
+else
+    filepath=$(wl-paste)
+fi
 
 # check if the filepath is absolute or relative, and if relative, make absolute
 if [ "${filepath#\/}" = "$filepath" ] || [ "${filepath#\~}" = "$filepath" ]; then
@@ -13,6 +18,9 @@ fi
 notify-send "$basename" "Taking screenshot to $filepath"
 
 sleep 5
-grim -g "$(slurp)" "$filepath"
 
-notify-send --icon "$filepath" "$basename" "Screenshot saved to: $filepath"
+if [ $(pgrep -x "Xorg") ]; then 
+    maim --select --hidecursor "$filepath" && notify-send --icon "$filepath" "$basename" "Screenshot saved to: $filepath"
+else
+    grim -g "$(slurp)" "$filepath"  && notify-send --icon "$filepath" "$basename" "Screenshot saved to: $filepath"
+fi
