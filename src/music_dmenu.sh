42b0219af823fdb3db8acc5739461642bb46fcd3
Andrew <andreasohaodha@proton.me>
2025-04-22 09:51:59 +0100

    [music_dmenu.sh]: Match case insensitively in dmenu

--- src/music_dmenu.sh
+++ src/music_dmenu.sh
@@ -3,19 +3,19 @@
 
 music_dir="$HOME/media/music"
 
-answer="$(printf "Artist\nAlbum\nTrack" | dmenu -p "Play music by:")"
+answer="$(printf "Artist\nAlbum\nTrack" | dmenu -i -p "Play music by:")"
 
-artist="$(ls "$music_dir" | dmenu -p "Artist:")"
+artist="$(ls "$music_dir" | dmenu -i -p "Artist:")"
 
 if [ "$answer" = "Artist" ]; then
     setsid umpv "$music_dir/$artist" &
 else
-    album="$(ls "$music_dir/$artist" | dmenu -p "Album:")"
+    album="$(ls "$music_dir/$artist" | dmenu -i -p "Album:")"
 
     if [ "$answer" = "Album" ]; then
         setsid umpv "$music_dir/$artist/$album" &
     else
-        track="$(ls "$music_dir/$artist/$album" | dmenu -p "Track:")"
+        track="$(ls "$music_dir/$artist/$album" | dmenu -i -p "Track:")"
         setsid umpv "$music_dir/$artist/$album/$track" &
     fi
 fi
